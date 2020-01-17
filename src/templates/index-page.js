3be8d659639aed45ca2c32d711980ebfed8a94c3
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-01-17 05:23:46 -0500

    Adjust map zoom level

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -209,7 +209,7 @@ export function IndexPageTemplate({ image, title, description }) {
           <Header as="h2">交通案内</Header>
           <iframe
             title="miyamadojinja"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1635.2858473869603!2d136.62029265745906!3d34.9422772304745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60038ecd8e3d43dd%3A0xc695f13a514d79d5!2sMiyamado%20Shrine!5e0!3m2!1sen!2sus!4v1579219856093!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d418628.8591278057!2d136.34374831595818!3d34.94312300000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc695f13a514d79d5!2sMiyamado%20Shrine!5e0!3m2!1sen!2sus!4v1579256375306!5m2!1sen!2sus"
             width="100%"
             height="450"
             frameborder="0"
